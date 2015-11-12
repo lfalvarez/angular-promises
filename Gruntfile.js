38250583b2291b8219167b08864a617826640ab9
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2015-11-12 17:08:01 -0300

    distributing

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! angular-promises`- v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> Ciudadano Inteligente;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['app/components/promises/category.js',
+              'app/components/promises/promise_model.js',
+              'app/components/promises/model_transformer.js',
+              'app/components/promises/categories_controller.js'
+        ],
+        dest: 'dist/angular-promises.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/angular-promises.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['app/component/*_test.js']
+      }
+    }
+  });
+  
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+};
---
+++ dist/angular-promises.js
@@ -0,0 +1,115 @@
+/*! angular-promises`- v0.0.0 - 2015-11-12
+* Copyright (c) 2015 Ciudadano Inteligente; Licensed  */
+'use strict';
+
+
+function slugify(text)
+{
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
+
+
+var Category = function(name){
+	this.name = name
+	this.slug = slugify(name)
+	this.promises = []
+}
+Category.prototype.add_promise = function(promise){
+	promise.category = this;
+	this.promises.push(promise);
+}
+
+angular.module('ddah.promises', ['ddah.promises.Promise'])
+
+.value("Category", Category);
+'use strict';
+
+var Prom = function(){
+    this.name = ''
+    this.description = ''
+    this.fulfillment = 0
+    this.ponderator = 1
+    this.quality = 1
+    this.category = null
+}
+Prom.prototype.ponderated_fulfillment = function(){
+    return this.fulfillment * this.ponderator
+}
+Prom.prototype.ponderated_quality = function(){
+    return this.quality * this.ponderator
+}
+
+angular.module('ddah.promises', [
+])
+
+.value("Promise", Prom);
+'use strict';
+
+
+var constructor = function(){
+
+}
+
+var promise_pusher= function(category, promise){
+	promise.category = category
+    category.promises.push(promise)
+    return category
+}
+
+var transformCategory = function(jsonResult){
+	var category = new Category(jsonResult['category']);
+    var promise = new Prom();
+    angular.extend(promise, jsonResult);
+    return promise_pusher(category, promise);
+}
+
+var getCategoryAndPromise = function(json_data){
+	var categories = []
+	angular.forEach(json_data, function(object) {
+		var category = transformCategory(object, constructor)
+		var position_in_array = categories.map(function(c) { return c.name; }).indexOf(category.name)
+		var exists =  position_in_array > -1;
+		if (!exists){
+	    	categories.push(category);
+		}
+		else {
+			var promise = category.promises[0];
+			categories[position_in_array] = promise_pusher(categories[position_in_array], promise)
+		}
+	}, this);
+	return categories;
+}
+
+var ddahTransformer = function(json_data){
+	if (!angular.isArray(json_data)) {
+		var temp = []
+		temp.push(json_data)
+
+		json_data = temp
+	}
+	return getCategoryAndPromise(json_data)
+}
+
+angular.module('ddah.promises', [
+	'ddah.promises.Promise',
+	'ddah.promises.Category'
+	]).factory("ddahTransformer", ddahTransformer);
+'use strict';
+
+
+angular.module('ddah.promises', [])
+var ddahAPP = angular.module('ddah',['ddah.promises']);
+
+ddahAPP.controller('DDAHCategoriesController', ['$scope', function($scope) {
+  if(typeof $scope.data === 'undefined'){
+  	$scope.categories = []
+  }
+  else {
+  	$scope.categories = ddahTransformer($scope.data)
+  }
+}]);
